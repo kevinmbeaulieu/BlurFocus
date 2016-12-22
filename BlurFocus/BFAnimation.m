2035aca65bff844de25418d6a6134e260bbff971
Kevin Beaulieu <kevinmbeaulieu@gmail.com>
2016-12-22 11:33:52 -0500

    Revert autorefresh feature

--- BlurFocus/BFAnimation.m
+++ BlurFocus/BFAnimation.m
@@ -13,13 +13,6 @@
 
 static NSString                 *blurType = @"CIGaussianBlur";
 static const double              blurRadius = 4.0;
-static const float               refreshRate = 1.0;
-
-@interface BFAnimation ()
-
-@property (nonatomic, retain) NSImageView *blurImageView;
-
-@end
 
 @implementation BFAnimation
 
@@ -28,26 +21,46 @@ static const float               refreshRate = 1.0;
     if (self = [super initWithDuration:duration animationCurve:animationCurve]) {
         _window = window;
         
+        // Grab snapshot of window
+        CGImageRef windowImageRef = CGWindowListCreateImage(
+                                                            CGRectNull, kCGWindowListOptionIncludingWindow,
+                                                            (CGWindowID)[_window windowNumber], kCGWindowImageBoundsIgnoreFraming);
+        CIImage *windowImage = [CIImage imageWithCGImage:windowImageRef];
         
+        // Blur snapshot, scale to fit
+        CIImage *blurredImage = [[windowImage imageByApplyingGaussianBlurWithSigma:blurRadius]
+                                 imageByCroppingToRect:CGRectMake(blurRadius, blurRadius,
+                                                                  windowImage.extent.size.width - blurRadius * 2,
+                                                                  windowImage.extent.size.height - blurRadius * 2)
+                                 ];
+        NSBitmapImageRep *blurredImageRep = [[NSBitmapImageRep alloc] initWithCIImage:blurredImage];
+        NSData *blurredImageData = [blurredImageRep representationUsingType:NSBitmapImageFileTypePNG
+                                                                 properties:@{}];
+        NSImage *blurredNSImage = [[NSImage alloc] initWithData:blurredImageData];
         
         // Create window with blurred image on top of target window
-        _overlayWindow = [[NSWindow alloc] initWithContentRect:_window.frame
-                styleMask:NSWindowStyleMaskBorderless backing:NSBackingStoreBuffered defer:NO];
-        _overlayWindow.backgroundColor = [NSColor clearColor];
-        _overlayWindow.opaque = NO;
-        _overlayWindow.alphaValue = 0.0f;
-        _overlayWindow.ignoresMouseEvents = YES;
-        _overlayWindow.contentView.wantsLayer = YES;
-        _overlayWindow.contentView.layer.opaque = NO;
-        _overlayWindow.contentView.layer.masksToBounds = YES;
-        _overlayWindow.contentView.layer.cornerRadius = MACOS_WINDOW_CORNER_RADIUS;
+        if (!_overlayWindow) {
+            _overlayWindow = [[NSWindow alloc] initWithContentRect:_window.frame
+                    styleMask:NSWindowStyleMaskBorderless backing:NSBackingStoreBuffered defer:NO];
+            _overlayWindow.backgroundColor = [NSColor clearColor];
+            _overlayWindow.opaque = NO;
+            _overlayWindow.alphaValue = 0.0f;
+            _overlayWindow.ignoresMouseEvents = YES;
+            _overlayWindow.contentView.wantsLayer = YES;
+            _overlayWindow.contentView.layer.opaque = NO;
+            _overlayWindow.contentView.layer.masksToBounds = YES;
+            _overlayWindow.contentView.layer.cornerRadius = MACOS_WINDOW_CORNER_RADIUS;
+        } else {
+            while (_overlayWindow.contentView.subviews.count > 0) {
+                [[_overlayWindow.contentView.subviews firstObject] removeFromSuperview];
+            }
+        }
+        
+        NSImageView *overlayView = [NSImageView imageViewWithImage:blurredNSImage];
+        overlayView.frame = NSMakeRect(0, 0, _overlayWindow.frame.size.width,
+                _overlayWindow.frame.size.height);
+        [_overlayWindow.contentView addSubview:overlayView];
         [_window addChildWindow:_overlayWindow ordered:NSWindowAbove];
-        
-        [self renderBlur];
-        
-        [NSTimer scheduledTimerWithTimeInterval:refreshRate target:self selector:@selector(renderBlur)
-                userInfo:nil repeats:YES];
-
     }
     return self;
 }
@@ -73,37 +86,6 @@ static const float               refreshRate = 1.0;
         }
     } else {
         _overlayWindow.alphaValue = currentProgress;
-    }
-}
-
-- (void)renderBlur {
-    // Grab snapshot of window
-    CGImageRef windowImageRef = CGWindowListCreateImage(
-                                                        CGRectNull, kCGWindowListOptionIncludingWindow,
-                                                        (CGWindowID)[_window windowNumber], kCGWindowImageBoundsIgnoreFraming);
-    CIImage *windowImage = [CIImage imageWithCGImage:windowImageRef];
-    
-    // Blur snapshot, scale to fit
-    CIImage *blurredImage = [[windowImage imageByApplyingGaussianBlurWithSigma:blurRadius]
-                             imageByCroppingToRect:CGRectMake(blurRadius, blurRadius,
-                                                              windowImage.extent.size.width - blurRadius * 2,
-                                                              windowImage.extent.size.height - blurRadius * 2)
-                             ];
-    NSBitmapImageRep *blurredImageRep = [[NSBitmapImageRep alloc] initWithCIImage:blurredImage];
-    NSData *blurredImageData = [blurredImageRep representationUsingType:NSBitmapImageFileTypePNG
-                                                             properties:@{}];
-    NSImage *blurredNSImage = [[NSImage alloc] initWithData:blurredImageData];
-    
-    // Update display
-    if (_blurImageView == nil) {
-        _blurImageView = [NSImageView imageViewWithImage:blurredNSImage];
-        _blurImageView.frame = NSMakeRect(0, 0, _overlayWindow.frame.size.width,
-                                          _overlayWindow.frame.size.height);
-        [_overlayWindow.contentView addSubview:_blurImageView];
-    } else {
-        _blurImageView.image = blurredNSImage;
-        _blurImageView.frame = NSMakeRect(0, 0, _overlayWindow.frame.size.width,
-                                         _overlayWindow.frame.size.height);
     }
 }
 
